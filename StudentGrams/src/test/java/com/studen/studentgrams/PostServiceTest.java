fe6c4ba01a80093ea6a200ce84d37c0a02f54a61
Yassien <y.boughlala@sutdent.fontys.nl>
2025-03-21 11:59:27 +0100

    correct java version

--- StudentGrams/src/test/java/com/studen/studentgrams/PostServiceTest.java
+++ StudentGrams/src/test/java/com/studen/studentgrams/PostServiceTest.java
@@ -18,7 +18,7 @@ import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PostServiceTest {
-
+//s//
     @InjectMocks
     private PostService postService;
 
